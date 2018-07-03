5b37e0e3eb721eb5106decd45f7819b4db1bfb0b
Frangu Vlad <contact@kingdgrizzle.ml>
2018-07-03 21:14:52 +0300

    :art: Basic Configs

---
+++ config.mjs
@@ -0,0 +1,24 @@
+export default {
+	/**
+	 * The token for the bot
+	 */
+	token: "",
+
+	/**
+	 * The owner/repository to get the doc JSONs from
+	 */
+	repository: "dirigeants/klasa",
+
+	/**
+	 * The branch that has the JSON documentation files
+	 */
+	jsonBranch: "docs",
+
+	/**
+	 * The branches to get the JSON from
+	 */
+	branches: [
+		"master",
+		"stable",
+	],
+};
