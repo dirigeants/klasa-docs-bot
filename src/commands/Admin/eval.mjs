3e61702ba2f15f6b1169c57f0937c4b91244bde6
Unknown <bdistin@gmail.com>
2018-07-26 21:27:56 -0500

    oops, old eval

--- src/commands/Admin/eval.mjs
+++ src/commands/Admin/eval.mjs
@@ -1,4 +1,5 @@
-import { Command, Stopwatch, Type } from 'klasa';
+import { Command, Stopwatch, Type, util } from 'klasa';
+import djs from 'discord.js';
 import { inspect } from 'util';
 
 export default class extends Command {
@@ -6,67 +7,69 @@ export default class extends Command {
 	constructor(...args) {
 		super(...args, {
 			aliases: ['ev'],
-			permLevel: 10,
+			permissionLevel: 10,
 			guarded: true,
 			description: 'Evaluates arbitrary Javascript.',
 			usage: '<expression:str>'
 		});
 	}
 
-	async run(msg, [code]) {
-		const { success, result, inspected, time, type } = await this.eval(msg, code);
-		const footer = this.client.methods.util.codeBlock('ts', type);
-		const silent = 'silent' in msg.flags;
+	async run(message, [code]) {
+		const { success, result, inspected, time, type } = await this.eval(message, code);
+		const footer = util.codeBlock('ts', type);
+		const silent = 'silent' in message.flags;
 
-		const output = new this.client.methods.Embed()
+		const output = new djs.MessageEmbed()
 			.setColor(success ? 0x00ff00 : 0xff0000)
 			.addField('Evaluates to:', result)
-			.addField(`${success ? 'Inspect' : 'Error'}:`, this.client.methods.util.codeBlock('js', inspected))
-			.addField('Type:', this.client.methods.util.codeBlock('ts', type))
+			.addField(`${success ? 'Inspect' : 'Error'}:`, util.codeBlock('js', inspected))
+			.addField('Type:', util.codeBlock('ts', type))
 			.setFooter(time);
 
 		// Handle errors
 		if (!success) {
 			if (result && result.stack) this.client.emit('error', result.stack);
-			if (!silent) return msg.sendMessage(output);
+			if (!silent) return message.sendMessage(output);
 		}
 
 		if (silent) return null;
 
 		// Handle too-long-messages
 		if (inspected.length > 1000) {
-			if (msg.guild && msg.channel.attachable) {
-				return msg.channel.sendFile(Buffer.from(inspected), 'output.txt', msg.language.get('COMMAND_EVAL_SENDFILE', time, footer));
+			if (message.guild && message.channel.attachable) {
+				return message.channel.sendFile(Buffer.from(inspected), 'output.txt', message.language.get('COMMAND_EVAL_SENDFILE', time, footer));
 			}
 			this.client.emit('log', result);
-			return msg.sendMessage(msg.language.get('COMMAND_EVAL_SENDCONSOLE', time, footer));
+			return message.sendMessage(message.language.get('COMMAND_EVAL_SENDCONSOLE', time, footer));
 		}
 
 		// If it's a message that can be sent correctly, send it
-		return msg.sendEmbed(output);
+		return message.sendEmbed(output);
 	}
 
 	// Eval the input
-	async eval(msg, code) {
+	async eval(message, code) {
+		// eslint-disable-next-line no-unused-vars
+		const msg = message;
 		const stopwatch = new Stopwatch();
 		let success, syncTime, asyncTime, result, inspected;
 		let thenable = false;
 		let type;
 		try {
-			if (msg.flags.async) code = `(async () => { ${code} })();`;
+			if (message.flags.async) code = `(async () => { ${code} })();`;
 			result = eval(code);
-			syncTime = stopwatch.friendlyDuration;
+			syncTime = stopwatch.toString();
 			type = new Type(result);
-			if (this.client.methods.util.isThenable(result)) {
+			if (util.isThenable(result)) {
 				thenable = true;
 				stopwatch.restart();
 				result = await result;
-				asyncTime = stopwatch.friendlyDuration;
+				asyncTime = stopwatch.toString();
 			}
 			success = true;
 		} catch (error) {
-			if (!syncTime) syncTime = stopwatch.friendlyDuration;
-			if (thenable && !asyncTime) asyncTime = stopwatch.friendlyDuration;
+			if (!syncTime) syncTime = stopwatch.toString();
+			if (thenable && !asyncTime) asyncTime = stopwatch.toString();
 			result = error;
 			success = false;
 		}
@@ -74,17 +77,17 @@ export default class extends Command {
 		stopwatch.stop();
 		if (success && typeof result !== 'string') {
 			inspected = inspect(result, {
-				depth: msg.flags.depth ? parseInt(msg.flags.depth) || 0 : 0,
-				showHidden: Boolean(msg.flags.showHidden)
+				depth: message.flags.depth ? parseInt(message.flags.depth) || 0 : 0,
+				showHidden: Boolean(message.flags.showHidden)
 			});
 		} else {
 			inspected = result.stack || result;
 		}
-		return { success, type, time: this.formatTime(syncTime, asyncTime), inspected, result: this.client.methods.util.clean(result) };
+		return { success, type, time: this.formatTime(syncTime, asyncTime), inspected, result: util.clean(String(result)) };
 	}
 
 	formatTime(syncTime, asyncTime) {
 		return asyncTime ? `⏱ ${asyncTime}<${syncTime}>` : `⏱ ${syncTime}`;
 	}
 
-}
+};
