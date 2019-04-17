5c39575352346857803ded9c5af9c564707234f7
Owen Bellish <shadowguy224@live.com>
2019-04-17 13:43:24 -0400

    partial done base

---
+++ src/lib/Client.ts
@@ -0,0 +1,3 @@
+import config from '../../config'
+const { branches, jsonBranch, repository, token } = config;
+import { Client } from 'klasa';
---
+++ src/lib/structures/base/BaseEntry.ts
@@ -0,0 +1,63 @@
+import { RichDisplay, Timestamp } from 'klasa';
+import { MessageEmbed } from 'discord.js';
+
+const TS = new Timestamp('DD.MM.YYYY [at] HH:mm:ss');
+
+export default class BaseEntry {
+    header: string;
+    content: string;
+    pages: any[];
+    documentation: any;
+    createdAt: string;
+    name: string;
+    branch: string;
+    prettyName: string;
+    parent: any;
+
+    constructor(documentation: { branch: string; }, string: any, createdAt: string | number | Date, name: string, parent: any, prettyName: string) {
+        this.header = '';
+        this.content = '';
+        this.pages = [];
+
+        this.documentation = documentation;
+        this.createdAt = TS.displayUTC(createdAt);
+        this.name = name;
+        this.parent = parent;
+        this.branch = documentation.branch;
+        this.prettyName = prettyName;
+
+        this._patch(string);
+    }
+
+    _patch(string) {
+        this.header = '';
+        this.content = '';
+        this.pages = [];
+
+        return string;
+    }
+
+    generateDisplay(color = 0x3669FA) {
+        const embed = new MessageEmbed()
+            .setColor(color)
+            .setThumbnail('https://cdn.discordapp.com/emojis/354702113147846666.png?v=1')
+            .setFooter(`Created ${this.createdAt}`)
+            .setTitle(this.prettyName)
+            .setURL(this._generateURL(this.name))
+        if (this.header.length) embed.setTitle(this.header);
+        const display = new RichDisplay(embed);
+        display.setFooterPrefix('ℹ Page ');
+        for (const { header, content } of this.pages) {
+            display.addPage((template: MessageEmbed) => {
+                if (header && header.length) template.setTitle(`${this.prettyName} :: ${header}`)
+                template.setDescription(content);
+                return template;
+            });
+        }
+        return display;
+    }
+
+    _generateURL(link = undefined) {
+        return `https://klasa.js.org/#/docs/klasa/${encodeURIComponent(this.branch)}/${encodeURIComponent(this.parent)}${link ? `/${encodeURIComponent(link)}` : ''}`;
+    }
+}
